250b7cc81914ca1b1dcab5310d62b47991449e89
Oscar <21753725a@iespoblenou.org>
2016-10-24 16:02:49 +0200

    Afegim la funció refresh al menú

--- app/src/main/java/com/example/a21753725a/magiccardlist/MainActivityFragment.java
+++ app/src/main/java/com/example/a21753725a/magiccardlist/MainActivityFragment.java
@@ -3,6 +3,7 @@ package com.example.a21753725a.magiccardlist;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -46,5 +47,24 @@ public class MainActivityFragment extends Fragment {
         cardList.setAdapter(adapter);
 
         return view;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            refresh();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void refresh() {
+
     }
 }
