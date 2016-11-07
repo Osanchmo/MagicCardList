4c4bc3794c5309ab71df8206208cf94d7234d1ff
Oscar <21753725a@iespoblenou.org>
2016-11-07 20:28:53 +0100

    Creem un intent per l'activity

--- app/src/main/java/com/example/a21753725a/magiccardlist/MainActivity.java
+++ app/src/main/java/com/example/a21753725a/magiccardlist/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.a21753725a.magiccardlist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,4 +16,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+
+                if (item.getItemId() == R.id.settings) {
+                    startActivity(new Intent(this, SettingsActivity.class));
+                    return true;
+                }
+                return super.onOptionsItemSelected(item);
+            }
 }
