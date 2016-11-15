47a4ab316fe63f560ae25ab4eabc18bf3191b9b6
Oscar <21753725a@iespoblenou.org>
2016-11-15 19:11:38 +0100

    pequeños arreglos del filtro

--- app/src/main/java/com/example/a21753725a/magiccardlist/SettingsActivity.java
+++ app/src/main/java/com/example/a21753725a/magiccardlist/SettingsActivity.java
@@ -173,10 +173,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             addPreferencesFromResource(R.xml.pref_general);
             setHasOptionsMenu(true);
 
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
             bindPreferenceSummaryToValue(findPreference("color"));
             bindPreferenceSummaryToValue(findPreference("rareza"));
         }
